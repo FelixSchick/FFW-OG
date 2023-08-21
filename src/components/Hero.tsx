882242788e5355545d5c451cd83a745b56a820ca
IllegalAccess <felix.schick@zyonic.de>
2023-08-21 22:22:22 +0200

    Added Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,11 @@
 import "./Hero.css";
+import hero from "../assets/hero.jpg";
 
 function Hero() {
-  return <div>Test</div>;
+  return (
+    <div className="heroSection">
+      <img className="heroBackgroundImg" src={hero} alt="Hero" />
+    </div>
+  );
 }
 export default Hero;
