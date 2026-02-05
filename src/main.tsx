17e4859f9fadcd31c8380fa14ad084ca4dcd974d
Felix Schick <schick.felix112@gmail.com>
2026-02-05 16:27:04 +0100

    feat: snow out commented

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Snowfall from 'react-snowfall';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+    {/*<Snowfall 
+     color="#ffffffff" 
+     snowflakeCount={400}
+     style={{ position: 'fixed', width: '100vw', height: '100vh', zIndex: 9999 }}
+   /> */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
